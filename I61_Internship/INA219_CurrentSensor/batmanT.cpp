60b487f7982d267666226ea925aca0cacbac806e
countryBumpkin <deepg2w@gmail.com>
2018-04-16 21:33:04 -0600

    Fixed erroneous call to logData(void)

--- I61_Internship/INA219_CurrentSensor/batmanT.cpp
+++ I61_Internship/INA219_CurrentSensor/batmanT.cpp
@@ -172,7 +172,7 @@ BatmanT::BatmanT(bool debugMode, bool verboseMode, bool limitLineMode, int numbe
 		//Save data...
 		if(verbose || debug){
 			std::cout << INFO << " saving data..." << std::endl;
-			logData(data);
+			logData();
 			collectedLines++;
 		}
 

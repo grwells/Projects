acbf06e01f70ba094b5dbaa4daac919f89405045
Garrett Wells <deepg2w@gmail.com>
2018-06-16 21:24:49 -0600

    New stuff

--- I61_Internship/Communication_System/CommSys.cpp
+++ I61_Internship/Communication_System/CommSys.cpp
@@ -1,6 +1,6 @@
 #include <CommSys.h>
 
-int CommSys::CommSys(bool radioOrFONA){
+CommSys::CommSys(bool radioOrFONA){
 	//TODO: Configure for radio or FONA modules
 	if(RADIO){
 		//TODO: set up the radio interface
@@ -8,4 +8,21 @@ int CommSys::CommSys(bool radioOrFONA){
 		//TODO: set up the FONA interface
 		FONA gsm;
 	}
+
 }
+
+CommSysCommSys(void){
+	//TODO: write default constructor
+}
+
+void CommSys::selectCommSys(bool radioOrFONA){}
+
+bool CommSys::sendMessage(std::string message){
+	//TODO: write send message for CommSys
+	return false;
+}
+
+bool CommSys::getMessage(void){
+	//TODO: write the get message for CommSys
+	return false;
+} 

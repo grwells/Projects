bb062d441c6d5c366de0db1f01aa207f130565ff
Garrett Wells <deepg2w@gmail.com>
2018-06-20 14:06:23 -0600

    Mopping it all up

---
+++ I61_Internship/INA219_CurrentSensor/hello.cpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   hello.cpp
+ * Author: deepg
+ *
+ * Created on April 25, 2018, 4:33 PM
+ */
+
+#include <iostream>
+#include <string.h>
+#include <wiringPi.h>
+
+#define HELLO "hello world"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << HELLO << endl;
+    return 0;
+}
+

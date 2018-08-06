46a888d16891a91899401ccf6faab27b03a2aa7c
Garrett Wells <deepg2w@gmail.com>
2018-08-06 15:25:38 -0600

    Added new code that is more modular :)

--- I61_Internship/Communication_System/FONA/FONA_test.cpp
+++ I61_Internship/Communication_System/FONA/FONA_test.cpp
@@ -3,59 +3,112 @@
 #include <string>
 #include <wiringSerial.h>
 
-int main(void){
+/*
+ * printDataAvail prints the data available over the serial connection
+ * @param int fd: The file descriptor for the device that the serial is connected to
+*/
+void printDataAvail(int fd){
+	int num = serialDataAvail(fd);
 
-	if(wiringPiSetup() == -1) std::cout << "ERROR: setup failed" << std::endl;
-	std::string dev = "/dev/ttyS0";
-	int fd = serialOpen(dev.c_str(), 115200);
-
-	if(fd < 0) std::cout << "ERROR: file descriptor < 0" << std::endl;
-	
-	std::string com = "AT";
-
-	std::cout << "Sending commands" << std::endl;	
-	serialPuts(fd, com.c_str());
-	std::cout << "Available data: " << serialDataAvail(fd) << std::endl;
-	delay(5000);
-
-	serialPuts(fd, com.c_str());
-	std::cout << "Available data: " << serialDataAvail(fd) << std::endl;
-	delay(5000);
-
-	serialPuts(fd, com.c_str());
-	std::cout << "Available data: " << serialDataAvail(fd) << std::endl;
-	delay(5000);
-
-	std::cout << "Sending printf" << std::endl;
-	serialPrintf(fd, com.c_str());
-	std::cout << "Available data: " << serialDataAvail(fd) << std::endl;
-
-	std::cout << "Getting available data" << std::endl;	
-	int charsAvail = serialDataAvail(fd);
-
-	char output[6] = {};
-
-	//TODO: Check to see why serialDataAvail is returning -1 after sending 'AT' 2x
-	std::cout << "chars available: " << charsAvail << std::endl;
-	if(charsAvail > 0){
-		for(int i = 0; i < charsAvail; i++){
-			output[i] = serialGetchar(fd);
-		}
-	}else {
-		std::cout << "Skipped loop" << std::endl;
+	for(int i = 0; i < num; i++){
+			std::cout << serialGetchar(fd);
 	}
 
-	std::cout << output << std::endl;
- 	
+	std::cout << std::endl;
 
-	com = "AT+CMGF=1\x0D";
-	serialPuts(fd, com.c_str());
+	serialFlush(fd);
 
-	com = "AT+CMGS=\"2088013217\"\x0D";
-	serialPuts(fd, com.c_str());
+}
 
-	std::string message = "Hello from FONA\x0D";
-	serialPuts(fd, message.c_str());
+void trySerialPuts(int fd){
+	std::string command = "AT\n\r";
 
+	serialPuts(fd, command.c_str());
+
+	int chars = serialDataAvail(fd);
+
+	std::cout << "Chars Available: " << chars << std::endl;
+
+	for(int i = 0; i < 2; i++){
+		std::cout << serialGetchar(fd);
+	}
+	std::cout << "\n";
+
+	serialFlush(fd);
+}
+
+void trySerialPutchar(int fd){
+	unsigned char command = 'A';
+
+	serialPutchar(fd, command);
+
+	command = 'T';
+
+	serialPutchar(fd, command);
+
+	int chars = serialDataAvail(fd);
+	std::cout << "Chars Available: " << chars << std::endl;
+
+	for(int i = 0; i < 2; i++){
+		std::cout << serialGetchar(fd);
+	}
+	std::cout << "\n";
+
+	serialFlush(fd);
+}
+
+void trySerialPrintf(int fd){
+	std::string command = "AT\n\r";
+
+	serialPrintf(fd, command.c_str());
+
+	int chars = serialDataAvail(fd);
+	std::cout << "Chars Available: " << chars << std::endl;
+
+	for(int i = 0; i < 2; i++){
+		std::cout << serialGetchar(fd);
+	}
+	std::cout << "\n";
+
+	serialFlush(fd);
+}
+
+int setup(void){
+	if(wiringPiSetup() == -1) std::cout << "ERROR: setup failed" << std::endl;
+	std::string dev = "/dev/ttyS0";
+	//std::string dev = "/dev/ttyUSB0";
+	int fd = serialOpen(dev.c_str(), 115200);
+	serialFlush(fd); //Flush the line to make sure it is clean
+
+	std::string command = "ATE0";
+
+	serialPuts(fd, command.c_str());
+
+	std::cout << "Chars Available: " << serialDataAvail(fd) << std::endl;
+	serialFlush(fd);
+
+	if(fd < 0) std::cout << "ERROR: file descriptor < 0" << std::endl;
+	return fd;	
+}
+
+int main(void){
+	
+	int fd = setup(); //Setup wiringPi and serial connections
+	
+	/*
+	 * TODO: Try printing "\n\r" to terminate commands
+	 * TODO: call serial flush after all output has beeb processed
+	 * TODO: Try the system read() and write() functions
+	*/
+
+	trySerialPuts(fd);
+	trySerialPutchar(fd);
+	trySerialPrintf(fd);
+
+	//std::cout << "\n";
+
+	serialFlush(fd);
+	
 	serialClose(fd);
+	return 0;
 }

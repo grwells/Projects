4c8464c86d6c16e920d41ec3cd01a21d96a20299
WellsFamily <WellsFamily@users-MBP.PK5001Z>
2018-08-06 15:16:41 -0600

    Added readH2O for only God knows what reason...

--- I61_Internship/Scheduler/Cron/program_scripts/readH2O.cpp
+++ I61_Internship/Scheduler/Cron/program_scripts/readH2O.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "/home/pi/Documents/GitHub/Personal/Projects/I61_Internship/HC-SR04_Ultrasonic/HC-SR04.h"
+#include "home/pi/Documents/GitHub/Personal/Projects/I61I61_Internship/Communication_System/FONA/FONA.h"
 
 void sendWarning(void){
    //TODO: send message to the user, print to debug file for now
@@ -15,16 +16,16 @@ void sendWarning(void){
 int main(void){
     //Read the sensor values and store them in a log file
     std::string filename = "/home/pi/Documents/GitHub/Personal/Projects/I61_Internship/Scheduler/Cron/program_scripts/waterlevel.log";
-    
+
     //Open the file to read
     std::ofstream file;
     file.open(filename);
-    
+
     //Initialize the sensor and read from it
     int echoPin = 4, trigPin = 5;
     HCSR04 hc(trigPin, echoPin);
-    float distance_toH2O = hc.distance(IMPERIAL);    
-    
+    float distance_toH2O = hc.distance(IMPERIAL);
+
     //Determine whether or not to send emergency message
     //TODO: read config file to get tank height
     int tank_height = 20;
@@ -33,7 +34,7 @@ int main(void){
     //Write the sensor values to the file
     //TODO: append the data, don't overwrite
     file << std::to_string(distance_toH2O);
-   
+
     //Return!
     return 1;
 }
